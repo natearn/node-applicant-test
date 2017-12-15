384389a6295ddf7e770b5c29c06242f9ab43f754
Nathan Arnold <natearn@natearn.info>
2017-12-15 16:37:29 +0100

    Create a test file
    It doesn't do anything yet!

---
+++ sender/test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const transmit = require('./transmit.js')
+const expect = require('expect')
+
+// make some assertions... but everything is async bleh
+
+// tests have passed
+console.log("All tests passed!")
