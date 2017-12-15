c3bb3f07fefad914941daed1ef5484f472baee75
Nathan Arnold <natearn@natearn.info>
2017-12-15 16:36:52 +0100

    Implement existing behaviour with Kefir

--- sender/transmit.js
+++ sender/transmit.js
@@ -1,13 +1,26 @@
 'use strict'
 
 const request = require('request')
+const Kefir = require('kefir')
 
-/*
-*  This function will be called for each event.  (eg: for each sensor reading)
-*  Modify it as needed.
-*/
-module.exports = function(eventMsg, encoding, callback) {
-  request.post('http://localhost:8080/event', {json: true, body: eventMsg}, (err, res, body) => {
-    callback(err)
-  })
+const events = Kefir.pool()
+const callbacks = Kefir.pool()
+
+const transmit = (message, encoding, callback) => {
+	callbacks.plug(Kefir.constant(callback)) // TODO: callback with errors
+	events.plug(Kefir.constant(message))
 }
+
+const post = (message,cb) => {
+	request.post('http://localhost:8080/event',
+		{json: true, body: message},
+		(err, res, body) => cb(err)
+	)
+}
+
+const callback = callbacks.toProperty(() => console.error)
+Kefir.combine([events],[callback])
+	.log()
+	.onValue(([m,f]) => post(m,f))
+
+module.exports = transmit
