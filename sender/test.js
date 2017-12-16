f1e6c7e0449e45bff301da471538f7bb12e7380d
Nathan Arnold <natearn@natearn.info>
2017-12-16 17:29:46 +0100

    Add at least one test for the sender

--- sender/test.js
+++ sender/test.js
@@ -2,8 +2,35 @@
 
 const transmit = require('./transmit.js')
 const expect = require('expect')
+const Kefir = require('kefir')
 
-// make some assertions... but everything is async bleh
+// Use Kefir to create mock scenarios
 
-// tests have passed
-console.log("All tests passed!")
+const message = {key: "value"}
+
+// event stream with long pause in the middle
+const longDelay = Kefir.concat([
+  Kefir.interval(100,message).take(10),
+  Kefir.later(3000,message),
+  Kefir.interval(100,message).take(10)
+])
+
+// copy the behaviour of transmit.js
+const runWith = (events) => {
+  const finished = events.debounce(2000)
+  return events
+    .bufferWithTimeOrCount(1500)           // Flush every 1.5 seconds
+    .filter((buffer) => buffer.length > 0) // Don't send empty messages
+    .takeUntilBy(finished)
+}
+
+// define a single test as a stream
+const testDelay = runWith(longDelay)
+  .bufferWhile() // capture all the output into a single array
+  .flatten()
+  .onValue(array => expect(array.length).toBe(10))
+
+// combine all the tests into a single test
+Kefir.merge([ testDelay ])
+  .onEnd(() => console.log("all tests passed"))
+
